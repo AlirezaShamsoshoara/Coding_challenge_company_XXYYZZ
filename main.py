1f03b8177a9353d60c84f9ba47d2bdda32106dcb
alireza <a.shamsoshoara@gmail.com>
2021-06-09 14:54:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
-Created on June 3rd, 2021
-@author:    Alireza Shamsoshoara
+Created on June 9th, 2021
+@author:    Alireza(Ali) Shamsoshoara
 @Project:   Coding Challenge for ResMed
 @Northern Arizona University
 This project is developed and tested with Python 3.6 using pycharm on Ubuntu 18.04 System
