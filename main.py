47aca0a174a76bc3cbede9521dd971117c510d98
Alireza <a.shamsoshoara@gmail.com>
2021-06-09 22:51:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ This project is developed and tested with Python 3.6 using pycharm on Ubuntu 18.
 # ############# import libraries
 # General Modules
 
+
+def main():
+    file_input = open('sample-input-1.txt', 'r')
+    all_lines = file_input.readlines()
+    for line in all_lines:
+        # print(line)
+        print(line.strip())
+        parts = line.rstrip().split('|')
+        date = parts[0]
+        value = parts[1]
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
